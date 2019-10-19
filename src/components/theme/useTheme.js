e51307c3e7edb5bc411f790e59902c2e62aa0f43
Dobrin Dobrev <dodoboy9620@gmail.com>
2019-10-20 01:39:37 +0300

    theme update

--- src/components/theme/useTheme.js
+++ src/components/theme/useTheme.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import storage from 'local-storage-fallback'
 
-function useTheme(defaultTheme = { mode: 'dark' }) {
+function useTheme(defaultTheme = { mode: 'light' }) {
   const [theme, _setTheme] = useState(getInitialTheme)
 
   function getInitialTheme() {
-    const savedTheme =
-      typeof window !== 'undefined' && window.localStorage.getItem('theme')
+    const savedTheme = storage.getItem('theme')
     return savedTheme ? JSON.parse(savedTheme) : defaultTheme
   }
 
   useEffect(() => {
-    typeof window !== 'undefined' &&
-      window.localStorage.setItem('theme', JSON.stringify(theme))
+    storage.setItem('theme', JSON.stringify(theme))
   }, [theme])
 
   return {
@@ -20,5 +18,4 @@ function useTheme(defaultTheme = { mode: 'dark' }) {
     setTheme: ({ setTheme, ...theme }) => _setTheme(theme),
   }
 }
-
 export default useTheme
