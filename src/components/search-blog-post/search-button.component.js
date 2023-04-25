dbf450c96f4d9ee5923a3834a33960b5ab28b420
topmonitoring <dodoboy9620@gmail.com>
2023-04-25 18:52:31 +0300

    updated search button

--- src/components/search-blog-post/search-button.component.js
+++ src/components/search-blog-post/search-button.component.js
@@ -1,7 +1,5 @@
-import React, {Children, useState} from 'react'
-import styled,{css} from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
-import {getImage,GatsbyImage} from 'gatsby-plugin-image'
+import React, { useState} from 'react'
+import styled from 'styled-components'
 import theme from 'styled-theming'
 import { Modal } from "react-responsive-modal";
 import "react-responsive-modal/styles.css";
@@ -42,27 +40,11 @@ const SearchButton =({children})=>{
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
-
-
-    const imageData=useStaticQuery(graphql`
-    {
-      blackSearchIcon: file(name: {eq: "search-interface-symbol-b"}) {
-        childImageSharp {
-          gatsbyImageData(quality: 100, width: 24, height: 24)
-        }
-      }
-      lightSearchIcon: file(name: {eq: "search-interface-symbol-w"}) {
-        childImageSharp {
-          gatsbyImageData(quality: 100, width: 24, height: 24)
-        }
-      }
-    }
-    `)
     return(
       <>
       <StyledButton onClick={onOpenModal} >
-      <StyledSearchButtonD image={getImage(imageData.blackSearchIcon)} alt='serch icon' />
-      <StyledSearchButtonL image={getImage(imageData.lightSearchIcon)} alt='serch icon' />
+      <StyledSearchButtonL viewBox="0 0 24 24" width="24px" height="24px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></StyledSearchButtonL>
+      <StyledSearchButtonD viewBox="0 0 24 24" width="24px" height="24px"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></StyledSearchButtonD>
       </StyledButton>
       
       <Modal
@@ -78,24 +60,24 @@ const SearchButton =({children})=>{
 
 export default SearchButton
 
-const StyledSearchButtonL = styled(GatsbyImage)`
-  display: ${getDispleyLight};
-  width: 20px;
-  height: 20px;
-  align-content: center;
-`
-const StyledSearchButtonD = styled(GatsbyImage)`
-  width: 20px;
-  height: 20px;
+
+const StyledSearchButtonL = styled.svg`
+  width: 24px;
+  height: 24px;
+  fill: black;
   display: ${getDispleyDark};
 `
+const StyledSearchButtonD = styled.svg`
+  width: 24px;
+  height: 24px;
+  fill: white;
+  display: ${getDispleyLight};
+`
 const StyledButton = styled.button`
-  width: 60px;
+  background-color: transparent;
   height: 40px;
   margin: auto 2px;
-  background:transparent;
-  border: none;
   cursor: pointer;
-  align-content: center;
+  border: none;
 `;
 
