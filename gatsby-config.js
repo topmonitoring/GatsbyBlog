490d3acf52aae533e1cdeb9ff5fe9a4d658d95ac
topmonitoring <dodoboy9620@gmail.com>
2023-08-09 17:37:49 +0300

    updated font preload

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,10 +69,11 @@ module.exports = {
     {
       resolve: `gatsby-omni-font-loader`,
       options: {
+        mode: "async",
         enableListener: true,
         preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
         web: [
-          {
+          { 
             name: `Open Sans`,
             file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
           },
